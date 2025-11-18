36b068ddf1a84aed3e3c777b8cb5f62577267ad5
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-11-19 04:31:46 +0500

    Прослушиватель местоположения пользователя.Дегтянников.

--- app/src/main/java/com/example/gps29_degtinnikov/MainActivity.java
+++ app/src/main/java/com/example/gps29_degtinnikov/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.gps29_degtinnikov;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.runtime.image.ImageProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+    LocationListener _locationListnaet = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if (location != null) {
+                mapView.getMap().move(
+                        new CameraPosition(
+                                new Point(location.getLatitude(), location.getLongitude()), 15,  0,  0));
+
+                mapView.getMap().getMapObjects().clear();
+                mapView.getMap().getMapObjects().addPlacemark(
+                        new Point(location.getLatitude(), location.getLongitude()),
+                        ImageProvider.fromResource(MainActivity.this, R.drawable.location)
+            );
+                GetAddressByGPS getAddressByGPS = new GetAddressByGPS(
+                        String.valueOf(location.getLongitude()) + "," + String.valueOf(location.getLatitude()),
+                        textAddress
+            );
+                getAddressByGPS.execute();
+            }
+        }
+    };
 }
