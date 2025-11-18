33906142f6afa25cd233aa03897439e8e030048e
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-11-19 04:39:03 +0500

    Прописанный метод onStart и onStop.Дегтянников.

--- app/src/main/java/com/example/gps29_degtinnikov/MainActivity.java
+++ app/src/main/java/com/example/gps29_degtinnikov/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gps29_degtinnikov;
 
+import android.Manifest;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
@@ -52,4 +54,33 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    1);
+        }
+        _LocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                 1000,
+               10,
+                _locationListnaet);
+        _LocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000,
+                 10,
+                _locationListnaet);
+        mapView.onStart();
+    }
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+    }
 }
