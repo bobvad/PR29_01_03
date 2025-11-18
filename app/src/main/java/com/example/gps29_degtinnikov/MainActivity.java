4661e4712cc0d2cf180273c9861e993eeedd64d2
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-11-19 04:35:53 +0500

    Прописанный метод onCreate.Дегтянников.

--- app/src/main/java/com/example/gps29_degtinnikov/MainActivity.java
+++ app/src/main/java/com/example/gps29_degtinnikov/MainActivity.java
@@ -2,23 +2,34 @@ package com.example.gps29_degtinnikov;
 
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
 import com.yandex.runtime.image.ImageProvider;
 
 public class MainActivity extends AppCompatActivity {
 
+    LocationManager _LocationManager;
+    TextView textAddress;
+    MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        MapKitFactory.setApiKey("6140d8d1-12ca-426b-b76e-65913c54854c");
+        MapKitFactory.initialize(this);
         setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapview);
+        textAddress = findViewById(R.id.edittext);
+        _LocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
     }
     LocationListener _locationListnaet = new LocationListener() {
         @Override
